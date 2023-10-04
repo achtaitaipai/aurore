b8cf6f63a638bf05e014c484ae0f29d5f24741eb
achtaitaipai <charles.cailleteau@gmail.com>
2023-10-04 11:35:42 +0200

    url with base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from '@astrojs/svelte'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://achtaitaipai.github.io',
-	base: 'aurore',
+	base: '/aurore/',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false
